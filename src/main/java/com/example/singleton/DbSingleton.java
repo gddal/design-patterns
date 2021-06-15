9941b7de1f294434a9678fe5067eb5b6cb71d3d9
gddal <goncaloda@gmail.com>
2021-06-15 23:30:11 +0100

    Made DbSingleton a thread safe class

--- src/main/java/com/example/singleton/DbSingleton.java
+++ src/main/java/com/example/singleton/DbSingleton.java
@@ -2,13 +2,23 @@ package com.example.singleton;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = null;
+    //make the variable thread safe
+    private static volatile DbSingleton instance = null;
 
-    private DbSingleton(){}
+    private DbSingleton(){
+        //protection against a java reflection class create an instance of this
+        if(instance != null)
+            throw new RuntimeException("Use getInstance() method to create");
+    }
 
     public static DbSingleton getInstance(){
-        if(instance == null)
-            instance = new DbSingleton();
+        if(instance == null){
+            synchronized(DbSingleton.class){
+                if(instance == null){
+                    instance = new DbSingleton();
+                }
+            }
+        }
         
             return instance ;
     }
